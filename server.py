71a4e6799cfa1cd59dbdebcac1561cc2e87e111d
nasrullaw74 <nasrullaw74@gmail.com>
2024-07-14 14:44:02 +0500

    last updated! 104

--- server.py
+++ server.py
@@ -17,23 +17,28 @@ def webhook():
 @app.route('/', methods=['GET'])
 def test():
     return '<h1>Success!</h1>'
-    # url = "http://194.195.119.84:8069"
-    # db = "kaashiboat"
-    # username = 'info@appeul.com'
-    # password = "jasoos74."
-    #
-    # common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
-    # # common.version()
-    #
-    # uid = common.authenticate(db, username, password, {})
-    # models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
-    # if uid:
-    #     product_id = models.execute_kw(db, uid, password, 'res.partner', 'search_read',
-    #                                    [[]], {'fields': ['id', 'name']})
-    #     print(product_id)
-    #     return '<h1>Success!</h1>'
-    # else:
-    #     return '<h1>Not Success!</h1>'
+
+
+
+@app.route('/get_parters', methods=['GET'])
+def get_partners():
+
+    url = "http://194.195.119.84:8069"
+    db = "demo"
+    username = 'info@appeul.com'
+    password = "jasoos74."
+
+    common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+    # common.version()
+
+    uid = common.authenticate(db, username, password, {})
+    models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+    if uid:
+        product_id = models.execute_kw(db, uid, password, 'res.partner', 'search_read',
+                                       [[]], {'fields': ['id', 'name']})
+        print(product_id)
+        return '<h1></h1>'
+
 
 
 
