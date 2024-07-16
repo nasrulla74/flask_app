aabb821368fcf1f56cf0d80c6f512ff12c0ef057
nasrullaw74 <nasrullaw74@gmail.com>
2024-07-16 18:09:58 +0500

    added create db function!

--- website/__init__.py
+++ website/__init__.py
@@ -22,8 +22,10 @@ def create_app():
 
     from .models import User, Note
 
-    with app.app_context():
-        db.create_all()
+    create_database(app)
+
+    # with app.app_context():
+    #     db.create_all()
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
